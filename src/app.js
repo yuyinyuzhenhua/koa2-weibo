1225035a1c8b3792b2395d42d82c7be7bc72d898
yuzhenhua <yuzhenhua@bjhike.com>
2020-08-02 18:13:54 +0800

    feat:404

--- src/app.js
+++ src/app.js
@@ -8,12 +8,20 @@ const logger = require('koa-logger')
 const session = require('koa-generic-session')
 const redisStore = require('koa-redis')
 const REDIS_CONF = require('./conf/db')
+const { isProd } = require('./utils/env')
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+// const index = require('./routes/index')
+// const users = require('./routes/users')
+const errorViewRouter = require('./routes/view/error')
 
 // error handler
-onerror(app)
+let onerrorConf = {};
+if(isProd){
+  onerrorConf = {
+    redirect: '/error'
+  } 
+}
+onerror(app, onerrorConf)
 
 // middlewares
 app.use(bodyparser({
@@ -50,8 +58,9 @@ app.use(session({
 }))
 
 // routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+// app.use(index.routes(), index.allowedMethods())
+// app.use(users.routes(), users.allowedMethods())
+app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
