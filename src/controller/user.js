07c80863d59ec37f27b30ac83613df16a517d285
yuzhenhua <yuzhenhua@bjhike.com>
2020-08-04 17:22:35 +0800

    feat: logout changeInfo

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,7 +1,8 @@
 const {
     getUserInfo,
     createUser,
-    deleteUser
+    deleteUser,
+    updateUser,
 } = require('../services/user')
 const { SuccessModel, ErrorModel } = require('../model/ResModel')
 const {
@@ -9,7 +10,9 @@ const {
     registerUserNameExistInfo,
     registerFailInfo,
     registerUserNameNotExistInfo,
-    deleteUserFailInfo
+    deleteUserFailInfo,
+    changeInfoFailInfo,
+    changePasswordFailInfo
 } = require('../model/ErrorInfo')
 const doCrypto = require('../utils/cryp')
 
@@ -74,9 +77,81 @@ async function deleteCurUser(userName){
 }
 
 
+/**
+ * 修改个人信息
+ * @param {Object} ctx ctx
+ * @param {string} nickName 昵称
+ * @param {string} city 城市
+ * @param {string} picture 头像
+ */
+async function changeInfo(ctx, { nickName, city, picture }){
+    const { userName } = ctx.session.userInfo
+    if (!nickName) {
+        nickName = userName
+    }    
+    const result = await updateUser(
+        {
+            newNickName: nickName,
+            newCity: city,
+            newPicture: picture
+        },
+        { userName }
+    )
+    if(result) {
+        Object.assign(ctx.session.userInfo, {
+            nickName,
+            city,
+            picture
+        })
+        return new SuccessModel()
+    }
+    return new ErrorModel(changeInfoFailInfo)
+}
+
+
+/**
+ * 修改密码
+ * @param {string} userName 用户名
+ * @param {string} password 当前密码
+ * @param {string} newPassword 新密码
+ */
+async function changePassword(userName, password, newPassword){
+    const result = await updateUser(
+        {
+            newPassword: doCrypto(newPassword)
+        },
+        {
+            userName,
+            password: doCrypto(password)
+        }
+    )
+    if (result) {
+        // 成功
+        return new SuccessModel()
+    }
+    // 失败
+    return new ErrorModel(changePasswordFailInfo)    
+}
+
+
+/**
+ * 退出登录
+ * @param {Object} ctx ctx
+ */
+async function logout(ctx){
+    delete ctx.session.userInfo
+    return new SuccessModel()
+
+}
+
+
+
 module.exports = {
     login,
     register,
     isExist,
-    deleteCurUser
+    deleteCurUser,
+    changeInfo,
+    changePassword,
+    logout
 }
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -7,7 +7,10 @@ const {
     login,
     register,
     isExist,
-    deleteCurUser
+    deleteCurUser,
+    changeInfo,
+    changePassword,
+    logout
 } = require('../../controller/user')
 const userValidate = require('../../validator/user')
 const { genValidator } = require('../../middlewares/validator')
@@ -40,7 +43,7 @@ router.post('/isExist', async (ctx, next) => {
     ctx.body = await isExist(userName);
 })
 
-
+// 测试环境下，测试账号登录之后，删除自己
 router.post('/delete', loginCheck, async (ctx, next) => {
     if(isTest){
         const { userName } = ctx.session.userInfo;
@@ -49,5 +52,25 @@ router.post('/delete', loginCheck, async (ctx, next) => {
         ctx.body = await deleteCurUser(userName)
     }
 })
+
+// 修改个人信息
+router.patch('/changeInfo', loginCheck, genValidator(userValidate), async (ctx, next) => {
+    const { nickName, city, picture } = ctx.request.body
+    ctx.body = await changeInfo(ctx, { nickName, city, picture })
+})
+
+// 修改密码
+router.patch('/changePassword', loginCheck, genValidator(userValidate), async (ctx, next) => {
+    const { password, newPassword } = ctx.request.body
+    const { userName } = ctx.session.userInfo
+    ctx.body = await changePassword(userName, password, newPassword)
+})
+
+// 退出登录
+router.post('/logout', loginCheck, async (ctx, next) => {
+    ctx.body = await logout(ctx)
+})
+
+
 
 module.exports = router
